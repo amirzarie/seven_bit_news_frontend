58c778ab7c69606428961124ca01c6997a3080ce
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2024-11-28 21:56:39 -0500

    now testing the final product on the server.

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -9,7 +9,8 @@ function MessageForm({ user }) {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/chat", {
+      // const res = await fetch("http://127.0.0.1:8000/api/chat", {
+      const res = await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -38,7 +39,8 @@ function MessageForm({ user }) {
 
   const handleReset = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/reset", {
+      // const res = await fetch("http://127.0.0.1:8000/api/reset", {
+        const res = await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/reset", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
