91ece8a3bf7db8434217b9e2db5f76a9da24d5cc
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2025-02-03 13:04:31 -0500

    updated frontend requests to send requests to the new cloud run backend.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ function App() {
     setIsLoading(true);
     try {
       const response = await fetch(
-        "https://backend-dot-seven-bit-news.nn.r.appspot.com/api/chat",
+        "https://seven-bit-news-207302804909.northamerica-northeast2.run.app/api/chat",
+        // "https://seven-bit-news-cgc35suboa-pd.a.run.app/api/chat",
+        // "https://backend-dot-seven-bit-news.nn.r.appspot.com/api/chat",
         // "http://localhost:8000/api/chat",
         {
           method: "POST",
@@ -82,7 +84,9 @@ function App() {
   const handleReset = async () => {
     setIsLoading(true);
     try {
-      await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/chat", {
+      await fetch("https://seven-bit-news-207302804909.northamerica-northeast2.run.app/api/chat", {
+      // await fetch("https://seven-bit-news-cgc35suboa-pd.a.run.app/api/chat", {
+      // await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/chat", {
       // await fetch("http://localhost:8000/api/chat", {
         method: "POST",
         headers: {
@@ -122,7 +126,9 @@ function App() {
 
   const fetchTrendingNews = async () => {
     try {
-      const response = await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/trending");
+      const response = await fetch("https://seven-bit-news-207302804909.northamerica-northeast2.run.app/api/trending");
+      // const response = await fetch("https://seven-bit-news-cgc35suboa-pd.a.run.app/api/trending");
+      // const response = await fetch("https://backend-dot-seven-bit-news.nn.r.appspot.com/api/trending");
       // const response = await fetch("http://localhost:8000/api/trending");
       const data = await response.json();
       setTrendingNews(data.trending_articles);
@@ -141,7 +147,9 @@ function App() {
     setIsLoading(true);
     try {
       const response = await fetch(
-        "https://backend-dot-seven-bit-news.nn.r.appspot.com/api/topic",
+        "https://seven-bit-news-207302804909.northamerica-northeast2.run.app/api/topic",
+        // "https://seven-bit-news-cgc35suboa-pd.a.run.app/api/topic",
+        // "https://backend-dot-seven-bit-news.nn.r.appspot.com/api/topic",
         // "http://localhost:8000/api/topic",
         {
           method: "POST",
